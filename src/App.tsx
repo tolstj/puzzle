27a899a51d4bcee71231622b433f96b88a9888e0
Nikita Tolstikov <Nikita.Tolstikov@noveogroup.com>
2022-07-05 23:44:29 +0400

    Add image uploader

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [image, setImage] = useState<null | string>(null);
+
+  const onSelectImage = (eventOnSelect: React.ChangeEvent<HTMLInputElement>) => {
+    if (eventOnSelect.target.files) {
+      const imageFile = eventOnSelect.target.files[0];
+      
+      const fileReader = new FileReader();
+      fileReader.onload = () => {
+        const result = fileReader.result as string;
+        if (result) {
+          setImage(result);
+        }
+      };
+    
+      fileReader.readAsDataURL(imageFile);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" accept="image/*" onChange={onSelectImage} />
+      {image && <img src={image} />}
+    </div>
+  );
+};
+
+export default App;
